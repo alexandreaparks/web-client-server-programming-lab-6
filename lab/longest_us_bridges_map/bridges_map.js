5ce6edd005ced93b8886cbe56235b76c3357cc4b
alexandrea parks <alparks@idk.com>
2024-03-07 23:39:05 -0600

    Finished Lab Part 1

--- lab/longest_us_bridges_map/bridges_map.js
+++ lab/longest_us_bridges_map/bridges_map.js
@@ -1,6 +1,6 @@
 // set center coordinates and oom level of map
 let usCenterCoordinates = [39.8282, -98.5795]
-let zoomLevel = 4
+let zoomLevel = 3  // 1 = whole world
 
 // create map
 let map = L.map('bridges-map').setView(usCenterCoordinates, zoomLevel)
@@ -19,11 +19,42 @@ bridges = [
     {"name": "Tacoma Narrows Bridge", "span": 853.44, "span_text": "853.44m", "city": "Tacoma - Kitsap", "location": [47.2690, -122.5517]}
 ]
 
+// find the longest bridge
+let longestBridge = bridges[0]
+
+// loop over every span and set the largest span found to be the longest bridge
+for (let x = 1 ; x < bridges.length ; x++) {
+    if (bridges[x].span > longestBridge.span) {
+        longestBridge = bridges[x]
+    }
+}
+
+// set up icons - one for longest bridge, and another for the remaining bridges
+// define icon class
+var BridgeIcon = L.Icon.extend({
+    options: {
+        iconSize:     [60, 60],
+    }
+});
+
+// use icon class to create both bridge icons
+var bridgeIcon = new BridgeIcon({iconUrl: 'bridge.png'}),
+    longestBridgeIcon = new BridgeIcon({iconUrl: 'longest_bridge_icon.png'})
+
 // loop over bridges array to create markers for each bridge object
 bridges.forEach( function(bridge) {
     let popUpText = `<b>${bridge.name}</b><br>City: ${bridge.city}<br>Span length: ${bridge.span_text}`
-    L.marker(bridge.location)
+    let iconObject = {}
+    if (bridge.name === longestBridge.name) {  // if it's the longest bridge
+        iconObject = {icon: longestBridgeIcon}
+    } else {
+        iconObject = {icon: bridgeIcon}
+    }
+
+    L.marker(bridge.location, iconObject)
         .bindPopup(popUpText)
         .addTo(map)
 })
+
+
 
